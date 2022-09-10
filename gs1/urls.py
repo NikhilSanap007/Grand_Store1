b0fc027a52ba29bcc4b715d31e37a26d357b40de
NikhilSanap007 <99248042+NikhilSanap007@users.noreply.github.com>
2022-09-10 22:21:12 +0530

    commit

--- gs1/urls.py
+++ gs1/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('contact', views.contact, name="contact"),
 
     #Add to cart
-    path('cart/add/<int:id>/', views.cart_add, name='cart_add'),
+    path('cart/cart_add/<int:id>/', views.cart_add, name='cart_add'),
     path('cart/item_clear/<int:id>/', views.item_clear, name='item_clear'),
     path('cart/item_increment/<int:id>/',
          views.item_increment, name='item_increment'),
